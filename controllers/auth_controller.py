ded629a893bbef579490aab8991541d9a48fabff
hsc996 <han.scaife@gmail.com>
2024-06-15 11:55:41 +1000

    auth register and login methods added

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request
 from init import bcrypt, db
 from sqlalchemy.exc import IntegrityError
 from psycopg2 import errorcodes
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
 
 from models.user import User, user_schema
 
@@ -38,6 +40,26 @@ def register_user():
             # unique violation
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {"error": "Email address already in use"}, 409
+
+@auth_bp.route("/login", methods=["POST"])
+def login_user():
+    # Get the data from the body of the request
+    body_data = request.get_json()
+
+    # Find the user in the DB with that email address
+    stmt = db.select(User).filter_by(email=body_data.get("email"))
+    user = db.session.scalar(stmt)
+
+    # If user exists and password is correct
+    if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
+        # Create JWT
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
+        # Repond back to the user
+        return {"email": user.email, "is_admin": user.is_admin, "token": token}
+    # Else
+    else:
+        # Rspond with error message
+        return {"error": "Invalid email or password"}, 401
 
 
 
